e5a26174d662d8d97e9ddb15868c1b42ab793e75
Eric Crawford <e2crawfo@uwaterloo.ca>
2014-04-15 09:28:04 -0400

    make_note.py now adds tags to notes.

--- make_note.py
+++ make_note.py
@@ -1,18 +1,30 @@
 
 from subprocess import call
 import datetime
+import argparse
+import string
 
 note_dir = '/home/e2crawfo/Dropbox/notes/'
+tag_marker = '%tag%'
 
-def make_note(name):
+def make_note(name, tags):
     date_time_string = str(datetime.datetime.now()).split('.')[0]
     date_time_string = reduce(lambda y,z: string.replace(y,z,"_"), [date_time_string,":","."," ","-"])
-    call(['vim', note_dir + "/" + name + "_" + date_time_string + ".md"])
 
+    filename = note_dir + "/" + name + "_" + date_time_string + ".md"
+    call(['vim', filename])
+
+    with open(filename, 'a') as f:
+        f.write('\n')
+
+        if tags:
+            for tag in tags:
+                f.write(tag_marker + tag + '\n')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Make a note.')
-    parser.add_argument('name', nargs='*', help="Name of the note.")
+    parser.add_argument('name', nargs=1, help="Name of the note.")
+    parser.add_argument('--tags', nargs='*', help="Tags for the note.")
     argvals = parser.parse_args()
 
-    view_notes(argvals.pattern[0])
+    make_note(argvals.name[0], argvals.tags)
